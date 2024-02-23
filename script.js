e776c6c956c4be27908899510de4f6c8ea523af9
ProfFerrer <profluciaferrer@gmail.com>
2024-02-23 15:55:27 -0300

    23feb 15:55

---
+++ script.js
@@ -0,0 +1,101 @@
+/* ================== Menú  - y Menú Hambburguesa  ================ */
+const nav = document.getElementById("nav");
+const abrir = document.getElementById("abrir");
+const cerrar = document.getElementById("cerrar");
+
+const menuInicio = document.getElementById("menu-inicio");
+const menuBalance = document.getElementById("menu-balance");
+const menuCategorias = document.getElementById("menu-categorias");
+const menuReportes = document.getElementById("menu-reportes");
+
+abrir.addEventListener("click", () => {
+	nav.classList.remove("hidden");
+	cerrar.classList.remove("hidden");
+	abrir.classList.add("hidden");
+});
+
+function cerrarNav() {
+	nav.classList.add("hidden");
+	cerrar.classList.add("hidden");
+	abrir.classList.remove("hidden");
+}
+cerrar.addEventListener("click", cerrarNav);
+menuInicio.addEventListener("click", cerrarNav);
+menuBalance.addEventListener("click", cerrarNav);
+menuCategorias.addEventListener("click", cerrarNav);
+menuReportes.addEventListener("click", cerrarNav);
+
+const contenedor_menuInicio = document.getElementById("cont-menu-inicio");
+const contenedor_menuBalance = document.getElementById("cont-menu-balance");
+const contenedor_menuCategorias = document.getElementById(
+	"cont-menu-categorias"
+);
+const contenedor_menuReportes = document.getElementById("cont-menu-reportes");
+
+function mostrar(mostrar) {
+	contenedor_menuInicio.classList.add("hidden");
+	contenedor_menuBalance.classList.add("hidden");
+	contenedor_menuCategorias.classList.add("hidden");
+	contenedor_menuReportes.classList.add("hidden");
+
+	mostrar.classList.remove("hidden");
+}
+
+menuInicio.addEventListener("click", () => {
+	mostrar(contenedor_menuInicio);
+});
+
+menuBalance.addEventListener("click", () => {
+	mostrar(contenedor_menuBalance);
+});
+
+menuCategorias.addEventListener("click", () => {
+	mostrar(contenedor_menuCategorias);
+});
+
+menuReportes.addEventListener("click", () => {
+	mostrar(contenedor_menuReportes);
+});
+
+/* ------------------------------------------------------------------------------------------------ */
+
+/* ======================== CLARO - OSCURO ========================  */
+const btn_claro_oscuro = document.getElementById("btn-claro-oscuro");
+
+btn_claro_oscuro.addEventListener("click", () => {
+	if (
+		localStorage.theme === "dark" || //esta en modo OSCURO? => PASAR A claro
+		(!("theme" in localStorage) &&
+			window.matchMedia("(prefers-color-scheme: dark)").matches)
+	) {
+		btn_claro_oscuro.innerHTML = <i class="fa fa-moon-o" aria-hidden="true"></i>; //poner luna
+		document.documentElement.classList.remove("dark"); //PASAR A claro
+		localStorage.theme = "light"; //PASAR A claro
+	} else {
+		btn_claro_oscuro.innerHTML = <i class="fa fa-sun-o"></i>; //poner SOL
+		document.documentElement.classList.add("dark"); //pasar a oscuro
+		localStorage.theme = "dark"; //pasar a oscuro
+	}
+});
+
+function modoClaroOscuro() {
+	if (
+		localStorage.theme === "dark" ||
+		(!("theme" in localStorage) &&
+			window.matchMedia("(prefers-color-scheme: dark)").matches)
+	) {
+		btn_claro_oscuro.innerHTML = <i class="fa fa-sun-o"></i>; //pone SOL
+		document.documentElement.classList.add("dark"); //pinta oscuro
+	} else {
+		btn_claro_oscuro.innerHTML = <i class="fa fa-moon-o" aria-hidden="true"></i>; //pone LUNA
+		document.documentElement.classList.remove("dark"); //pinta Claro
+	}
+}
+/* ------------------------------------------------------------------------------------------------ */
+
+/* ================================================================================================*/
+function funcionesAEjecutar() {
+	modoClaroOscuro();
+}
+/* Cuando se termina de cargar la página  */
+window.onload = funcionesAEjecutar;
